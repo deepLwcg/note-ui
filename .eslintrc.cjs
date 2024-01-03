cf249d148f096678aa0d340af072a4fcc85f9fec
wangchenguang <wangchenguang@nullht.com>
2024-01-03 18:38:57 +0800

    first commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,53 +1,119 @@
+/* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution")
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
     },
-    "extends": [
+    globals: {
+        $: "readonly",
+        $$: "readonly",
+        $ref: "readonly",
+        $shallowRef: "readonly",
+        $computed: "readonly"
+    },
+    extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:vue/vue3-essential",
-        "@vue/eslint-config-typescript"
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-typescript/recommended",
+        "@vue/eslint-config-prettier"
     ],
-    "overrides": [
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+        ecmaVersion: 2020,
+        sourceType: "module",
+        jsxPragma: "React",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    overrides: [
         {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
+            files: ["*.ts", "*.vue"],
+            rules: {
+                "no-undef": "off"
             }
         },
         {
-            "files": ["*.vue"],
-            "parser": "vue-eslint-parser",
-            "parserOptions": {
-                "parser": "@typescript-eslint/parser",
-                "extraFileExtensions": [".vue"],
-                "ecmaVersion": "latest",
-                "ecmaFeatures": {
-                    "jsx": true
+            files: ["*.vue"],
+            parser: "vue-eslint-parser",
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                extraFileExtensions: [".vue"],
+                ecmaVersion: "latest",
+                ecmaFeatures: {
+                    jsx: true
                 }
             },
-            "rules": {
+            rules: {
                 "no-undef": "off"
+            }
+        },
+        {
+            env: {
+                node: true
+            },
+            files: [
+                ".eslintrc.{js,cjs}"
+            ],
+            parserOptions: {
+                sourceType: "script"
             }
         }
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
+    plugins: [
         "@typescript-eslint",
         "vue"
     ],
-    "rules": {
+    rules: {
+        "vue/no-v-html": "off",
+        "vue/require-default-prop": "off",
+        "vue/require-explicit-emits": "off",
+        "vue/multi-word-component-names": "off",
+        "@typescript-eslint/no-explicit-any": "off", // any
+        "no-debugger": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off", // setup()
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "vue/html-self-closing": [
+            "error",
+            {
+                html: {
+                    void: "always",
+                    normal: "always",
+                    component: "always"
+                },
+                svg: "always",
+                math: "always"
+            }
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_"
+            }
+        ],
+        "no-unused-vars": [
+            "error",
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_"
+            }
+        ],
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto"
+            }
+        ]
     }
 }
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,9 @@
 module.exports = {
-
-}
+  bracketSpacing: true, //在对象前后添加空格-eg: { foo: bar }
+  arrowParens: 'always', //单参数箭头函数参数周围使用圆括号-eg: (x) => x
+  trailingComma: 'all', //多行时尽可能打印尾随逗号
+  semi: true, //句末使用分号
+  singleQuote: true, //使用单引号
+  jsxSingleQuote: true, // jsx中使用单引号
+  embeddedLanguageFormatting: 'auto', //对引用代码进行格式化
+};
