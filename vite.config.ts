d3fd545752cc2604a748325b8f97e4995899a718
wangchenguang <wangchenguang@nullht.com>
2024-01-03 19:01:34 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { resolve } from 'path';
+import removeConsole from 'vite-plugin-remove-console';
+
+const root: string = process.cwd();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  root: root,
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  },
+  plugins: [vue(), vueJsx(), removeConsole()],
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 4000,
   },
 });
